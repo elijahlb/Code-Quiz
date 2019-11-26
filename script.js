e434208045efc9c23a5dc8a3200008afc65fc52b
Elijah Broaddus <elijahlb@gmail.com>
2019-11-26 13:22:52 -0500

    questions added

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
 var questions = [
-    {            :         },
-    {            :         },
-    {            :         },
-    {            :         },
-    {            :         }
+    {   
+        title : "Which of the following is correct about features of JavaScript?",
+        choices : ["JavaScript is a lightweight, interpreted programming language.", 
+        "JavaScript is designed for creating network-centric applications.", 
+        "JavaScript is complementary to and integrated with Java.", "All of the above."],
+        answer : "All of the above."
+    },
+    {   
+        title : " Which of the following is a valid type of function javascript supports?",
+        choices : ["named function", "anonymous function", "Both of the above", "None of the above"],
+        answer : "Both of the above"
+    },
+    {   
+        title : "Which built-in method returns the index within the calling String object of the first occurrence of the specified value?",
+        choices : ["getIndex()", "location()", "indexOf()", "None of the above"],
+        answer : "indexOf()"
+    },
+    {   
+        title : "Which built-in method returns the characters in a string beginning at the specified location?",
+        choices : ["substr()", "getSubstring()", "slice()", "None of the above"],
+        answer : "substr()"
+    },
+    {   
+        title : "Which of the following function of Boolean object returns a string of either 'true' or 'false' depending upon the value of the object?",
+        choices : ["toSource", "valueOf()", "toString()", "None of the above"],
+        answer : "toString()"
+    },
+    {   
+        title : "Which of the following function of String object splits a String object into an array of strings by separating the string into substrings?",
+        choices : ["slice()", "split()", "replace()", "search()"],
+        answer : "split()"
+    },
+    {   
+        title : "Which of the following function of String object returns the calling string value converted to lower case?",
+        choices : ["toLocaleLowerCase()", "toLowerCase()", "toString()", "substring()"],
+        answer : "toLowerCase"
+    },
+    {   
+        title : "Which of the following function of String object causes a string to be displayed as struck-out text, as if it were in a <strike> tag?",
+        choices : ["sup()", "small()", "strike()", "sub()"],
+        answer : "strike()"
+    },
+    {   
+        title : "Which of the following function of Array object returns true if every element in this array satisfies the provided testing function?",
+        choices : ["concat()", "every()", "push()", "some()"],
+        answer : "every()"
+    },
+    {   
+        title : "Which of the following function of Array object applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value?",
+        choices : ["pop()", "push()", "reduce()", "reduceRight()"],
+        answer : "reduceRight()"
+    },
 ]

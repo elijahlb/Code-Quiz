868561c696ff7108f1083f160093d38af040d903
Elijah Broaddus <elijahlb@gmail.com>
2019-12-02 18:41:07 -0500

    fixed javascript

--- script.js
+++ script.js
@@ -1,90 +1,54 @@
-var startButton = document.getElementById("start-btn")
-var questionContainer = document.getElementById("question-container")
-var nextButton = document.getElementById("next-btn")
-var answerButton1 = document.getElementById("ansBtn1")
-var answerButton2 = document.getElementById("ansBtn2")
-var answerButton3 = document.getElementById("ansBtn3")
-var answerButton4 = document.getElementById("ansBtn4")
-var question = document.getElementById("question")
-var userAnswer = document.querySelector(".ansButton")
+var startButton = document.getElementById("start-btn");
+var questionContainer = document.getElementById("question-container");
+var nextButton = document.getElementById("next-btn");
+var answerButton1 = document.getElementById("0");
+var answerButton2 = document.getElementById("1");
+var answerButton3 = document.getElementById("2");
+var answerButton4 = document.getElementById("3");
+var question = document.getElementById("question");
+var userAnswer = document.querySelector(".ansButton");
+var timer = 150;
+var score = 0;
+
 
 
 startButton.addEventListener("click", startGame);
 
 
-
 function startGame (){
     startButton.classList.add("hide");
     questionContainer.classList.remove("hide");
-
+    questionContainer.textContent = questions[0]
     
+
 }
 
+for (var i=0;i<questions.length;i++) {
+    questionContainer.textContent = questions[0]
 
 
 
-var questions = [
-    {   
-        title : "Which of the following is correct about features of JavaScript?",
-        choices : ["JavaScript is a lightweight, interpreted programming language.", 
-        "JavaScript is designed for creating network-centric applications.", 
-        "JavaScript is complementary to and integrated with Java.", "All of the above."],
-        answer : "All of the above."
-    },
-    {   
-        title : " Which of the following is a valid type of function javascript supports?",
-        choices : ["named function", "anonymous function", "Both of the above", "None of the above"],
-        answer : "Both of the above"
-    },
-    {   
-        title : "Which built-in method returns the index within the calling String object of the first occurrence of the specified value?",
-        choices : ["getIndex()", "location()", "indexOf()", "None of the above"],
-        answer : "indexOf()"
-    },
-    {   
-        title : "Which built-in method returns the characters in a string beginning at the specified location?",
-        choices : ["substr()", "getSubstring()", "slice()", "None of the above"],
-        answer : "substr()"
-    },
-    {   
-        title : "Which of the following function of Boolean object returns a string of either 'true' or 'false' depending upon the value of the object?",
-        choices : ["toSource", "valueOf()", "toString()", "None of the above"],
-        answer : "toString()"
-    },
-    {   
-        title : "Which of the following function of String object splits a String object into an array of strings by separating the string into substrings?",
-        choices : ["slice()", "split()", "replace()", "search()"],
-        answer : "split()"
-    },
-    {   
-        title : "Which of the following function of String object returns the calling string value converted to lower case?",
-        choices : ["toLocaleLowerCase()", "toLowerCase()", "toString()", "substring()"],
-        answer : "toLowerCase"
-    },
-    {   
-        title : "Which of the following function of String object causes a string to be displayed as struck-out text, as if it were in a <strike> tag?",
-        choices : ["sup()", "small()", "strike()", "sub()"],
-        answer : "strike()"
-    },
-    {   
-        title : "Which of the following function of Array object returns true if every element in this array satisfies the provided testing function?",
-        choices : ["concat()", "every()", "push()", "some()"],
-        answer : "every()"
-    },
-    {   
-        title : "Which of the following function of Array object applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value?",
-        choices : ["pop()", "push()", "reduce()", "reduceRight()"],
-        answer : "reduceRight()"
-    },
-]
 
-for (var i=0;i<choice.choices.length;i++) {
-    answerButton1.textContent = choice.choices[0]
-    answerButton2.textContent = choice.choices[1]
-    answerButton3.textContent = choice.choices[2]
-    answerButton4.textContent = choice.choices[3]
-    question.textContent = questions.q
-
+    
     
    
 }
+
+function checkAnswer () {
+    if ( choice.choices.clicked_id === answers.a) {
+        console.log("correct")
+    } else {
+        console.log("incorrect")
+    }
+
+}
+
+/*listEl.addEventListener("click", function(event) {
+    event.preventDefault();
+    if(event.target.matches("button")) {
+      var item = document.createElement("div");
+      item.textContent = groceries[event.target.parentElement.id];
+      shoppingCartEl.append(item);
+    }
+  });*/
+

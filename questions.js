0d0b492fa0938982049827877fe10e0b97a3044f
Elijah Broaddus <elijahlb@gmail.com>
2019-12-03 18:08:33 -0500

    working questions and answers

--- questions.js
+++ questions.js
@@ -1,31 +1,3 @@
-/*var questions = [
-    "Which of the following is correct about features of JavaScript?",
-    " Which of the following is a valid type of function javascript supports?",
-    "Which built-in method returns the index within the calling String object of the first occurrence of the specified value?",
-    "Which built-in method returns the characters in a string beginning at the specified location?",
-    "Which of the following function of Boolean object returns a string of either 'true' or 'false' depending upon the value of the object?",
-    "Which of the following function of String object splits a String object into an array of strings by separating the string into substrings?",
-    "Which of the following function of String object returns the calling string value converted to lower case?",
-    "Which of the following function of String object causes a string to be displayed as struck-out text, as if it were in a <strike> tag?",
-    "Which of the following function of Array object returns true if every element in this array satisfies the provided testing function?",
-   "Which of the following function of Array object applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value?"
-];
-
-var choice = [
-["JavaScript is a lightweight, interpreted programming language.", "JavaScript is designed for creating network-centric applications.", "JavaScript is complementary to and integrated with Java.", "All of the above."],
- ["named function", "anonymous function", "Both of the above", "None of the above"],
-["getIndex()", "location()", "indexOf()", "None of the above"],
-["substr()", "getSubstring()", "slice()", "None of the above"],
- ["toSource()", "valueOf()", "toString()", "None of the above"],
-["slice()", "split()", "replace()", "search()"],
-["toLocaleLowerCase()", "toLowerCase()", "toString()", "substring()"],
- ["concat()", "every()", "push()", "some()"],
-["pop()", "push()", "reduce()", "reduceRight()"]
-];
-
-var answers = [3, 2, 2, 0, 2, 0, 1, 2, 1, 3];
-*/
-
 var questions = [
    {
       title: " Which of the following is a valid type of function javascript supports?",
--- script.js
+++ script.js
@@ -11,55 +11,7 @@ var newButtons = document.getElementById("answer-buttons");
 var validate = document.getElementById("validate");
 var button = document.getElementById("button");
 var score = parseInt(document.getElementById("score").innerHTML);
-
-/*startButton.addEventListener("click", startGame);
-
-
-function startGame (){
-    startButton.classList.add("hide");
-    questionContainer.classList.remove("hide");
-    question.textContent = questions[0];
-    answerButton1.textContent = choice[0][0];
-    answerButton2.textContent = choice[0][1];
-    answerButton3.textContent = choice[0][2];
-    answerButton4.textContent = choice[0][3];
-    
-};
-
-
-    function replyClick(clicked_id){
-    for (var i=0; i<answers.length; i++) {
-    }
-    if (clicked_id == answers[0]) {
-        score+=10;
-        document.getElementById("score").innerHTML = score;
-        validate.classList.add("correct")
-        validate.innerHTML = "Correct"
-        nextButton.classList.remove("hide");
-    } else if (clicked_id != answers[0] && score == 0) {
-        document.getElementById("score").innerHTML = score;
-        validate.classList.add("incorrect")
-        validate.innerHTML = "Incorrect"
-        nextButton.classList.remove("hide");
-    } else {
-        score-=10;
-        document.getElementById("score").innerHTML = score;
-        validate.classList.add("incorrect")
-        validate.innerHTML = "Incorrect"
-        nextButton.classList.remove("hide");
-    }
-    };
-
-    nextButton.addEventListener("click", next)
-
-    function next() {
-        validate.innerHTML = ""
-        nextButton.classList.add("hide");
-        for (var i=1; i<questions.length; i++) {
-            question.innerHTML = questions[i];
-        }
-       
-    };*/
+var index = 0;
 
     startButton.addEventListener("click", startGame);
 
@@ -67,31 +19,39 @@ function startGame (){
     function startGame (){
     startButton.classList.add("hide");
     questionContainer.classList.remove("hide");
-    for (var i=0;i<questions.length;i++) {
-    question.textContent = questions[0].title;
-    answerButton1.textContent = questions[0].choices[0];
-    answerButton2.textContent = questions[0].choices[1];
-    answerButton3.textContent = questions[0].choices[2];
-    answerButton4.textContent = questions[0].choices[3];
-    }
+    question.textContent = questions[index].title;
+    answerButton1.textContent = questions[index].choices[0];
+    answerButton2.textContent = questions[index].choices[1];
+    answerButton3.textContent = questions[index].choices[2];
+    answerButton4.textContent = questions[index].choices[3];  
 };
+
+    nextButton.addEventListener("click", next)
+
+    function next() {
+        question.textContent = questions[index+=1].title;
+        answerButton1.textContent = questions[index].choices[0];
+        answerButton2.textContent = questions[index].choices[1];
+        answerButton3.textContent = questions[index].choices[2];
+        answerButton4.textContent = questions[index].choices[3]; 
+    };
 
 
     function replyClick(clicked_id){
     
-    if (clicked_id == questions[0].answer) {
-        score+=10;
+    if (clicked_id == questions[index].answer) {
+        score+=1;
         document.getElementById("score").innerHTML = score;
         validate.classList.add("correct")
         validate.innerHTML = "Correct"
         nextButton.classList.remove("hide");
-    } else if (clicked_id != questions[0].answer && score == 0) {
+    } else if (clicked_id != questions[index].answer && score == 0) {
         document.getElementById("score").innerHTML = score;
         validate.classList.add("incorrect")
         validate.innerHTML = "Incorrect"
         nextButton.classList.remove("hide");
     } else {
-        score-=10;
+        score-=1;
         document.getElementById("score").innerHTML = score;
         validate.classList.add("incorrect")
         validate.innerHTML = "Incorrect"
@@ -99,16 +59,7 @@ function startGame (){
     }
     };
 
-    nextButton.addEventListener("click", next)
-
-    function next() {
-        validate.innerHTML = ""
-        nextButton.classList.add("hide");
-        for (var i=1; i++;) {
-            alert(questions[i])
-        }
-       
-    };
+   
 
 
 
